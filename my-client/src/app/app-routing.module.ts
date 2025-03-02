c8f95ba90c50e980dc0315981c873c5a4b10683b
qnhu11 <nhuvq22411c@st.uel.edu.vn>
2025-03-02 13:34:02 +0700

    initial commit

--- my-client/src/app/app-routing.module.ts
+++ my-client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 const routes: Routes = [
-  {path:"contact-page",component:ContactComponent}
+  {path:"contact-page",component:ContactComponent},
+  {path:"sidebar-page",component:SidebarComponent}
 ];
 
 @NgModule({
