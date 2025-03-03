a8eaadb56f16853fd869693de16160f25015094f
Nhung Pham Tuyet <131536332+nhungnehehe@users.noreply.github.com>
2025-03-03 11:20:41 +0700

    initial commit

--- my-client/src/app/payment/payment.component.ts
+++ my-client/src/app/payment/payment.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-payment',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
+  email: string = '';
+  fullName: string = '';
+  street: string = '';
+  selectedCity: string = '';
+  district: string = '';
+  phone: string = '';
 
+  cities: any[] = [];
+  orderItems = [
+    { image: 'image.png', name: 'Cement Coffee Table', price: '$3,982', quantity: 1, color: 'Light Beige' },
+    { image: 'image.png', name: 'Cement Coffee Table', price: '$3,982', quantity: 1, color: 'Light Beige' }
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getCities();
+  }
+
+  getCities(): void {
+    this.http.get<any[]>('https://provinces.open-api.vn/api/p/').subscribe(
+      (data) => {
+        this.cities = data;
+      },
+      (error) => {
+        console.error('Lỗi khi lấy danh sách tỉnh/thành phố:', error);
+      }
+    );
+  }
 }
