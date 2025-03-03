2f77cd3cedd6aa8f29be882cb3a6170f6fd1e4f0
Nhung Pham Tuyet <131536332+nhungnehehe@users.noreply.github.com>
2025-03-03 11:53:23 +0700

    initial commit

--- my-client/src/app/app-routing.module.ts
+++ my-client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ContactComponent } from './contact/contact.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { CartComponent } from './cart/cart.component';
 import { BlogComponent } from './blog/blog.component';
+import { PaymentComponent } from './payment/payment.component';
 
 const routes: Routes = [
   {path:"contact-page",component:ContactComponent},
   {path:"sidebar-page",component:SidebarComponent},
   {path:"cart-page",component:CartComponent},
-  {path:"blog-page",component:BlogComponent}
+  {path:"blog-page",component:BlogComponent},
+  {path:"about-page",component:PaymentComponent}
 ];
 
 @NgModule({
