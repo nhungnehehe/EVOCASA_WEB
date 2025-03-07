d57897e2a83819e200a4d29f89f8d1e9acde9f15
qnhu11 <nhuvq22411c@st.uel.edu.vn>
2025-03-07 20:38:35 +0700

    initial commit

--- my-admin/src/app/customer/customer.component.ts
+++ my-admin/src/app/customer/customer.component.ts
@@ -12,6 +12,7 @@ export class CustomerComponent {
     { id: '123456', name: 'Mark', gender: 'Male', email: 'mark@gmail.com', phone: '0832-XXX-XXX', amount: '1000$' },
     { id: '123457', name: 'John', gender: 'Male', email: 'john@gmail.com', phone: '0832-XXX-XXX', amount: '2000$' },
     { id: '123458', name: 'Anna', gender: 'Female', email: 'anna@gmail.com', phone: '0832-XXX-XXX', amount: '1500$' },
+    { id: '123459', name: 'Emma', gender: 'Female', email: 'emma@gmail.com', phone: '0832-XXX-XXX', amount: '1800$' },
     { id: '123459', name: 'Emma', gender: 'Female', email: 'emma@gmail.com', phone: '0832-XXX-XXX', amount: '1800$' }
   ];
 
