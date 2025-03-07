f647ed78c53e1f2d66517d8f2d20e75df2d21e01
qnhu11 <nhuvq22411c@st.uel.edu.vn>
2025-03-07 15:34:35 +0700

    initial commit

--- my-admin/src/app/app-routing.module.ts
+++ my-admin/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { CustomerComponent } from './customer/customer.component';
 import { ProductComponent } from './product/product.component';
 import { CategoryComponent } from './category/category.component';
 import { OrderComponent } from './order/order.component';
+import { CustomerComponent } from './customer/customer.component';
 
 const routes: Routes = [
   { path: "", redirectTo:"dashboard", pathMatch:"full"},
