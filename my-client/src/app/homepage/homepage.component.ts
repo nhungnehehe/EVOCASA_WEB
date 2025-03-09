d148627a235f01d45b062e6ad7ef81f2e3db4bc9
Lê Nguyễn Minh Thảo <thaolnm22411c@st.uel.edu.vn>
2025-03-09 11:03:22 +0700

    initial commit

--- my-client/src/app/homepage/homepage.component.ts
+++ my-client/src/app/homepage/homepage.component.ts
@@ -7,38 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
-  banners = [
-    { image: 'assets/banner1.jpg' },
-    { image: 'assets/banner2.jpg' },
-    { image: 'assets/banner3.jpg' }
-  ];
   
-  currentIndex = 0;
-  totalSlides = this.banners.length;
-  slideWidth = 1525;
-  interval: any;
-
-  ngOnInit() {
-    this.startAutoSlide();
-  }
-
-  showSlide(index: number) {
-    this.currentIndex = index;
-  }
-
-  nextSlide() {
-    this.currentIndex = (this.currentIndex + 1) % this.totalSlides;
-  }
-
-  startAutoSlide() {
-    this.interval = setInterval(() => {
-      this.nextSlide();
-    }, 4000);
-  }
-
-  ngOnDestroy() {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-  }
 }
