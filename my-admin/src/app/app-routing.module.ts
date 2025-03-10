4cda5bb9cf1e6f564d253c6be6b9ca1f88806b2d
Nhung Pham Tuyet <131536332+nhungnehehe@users.noreply.github.com>
2025-03-10 23:40:09 +0700

    initial commit

--- my-admin/src/app/app-routing.module.ts
+++ my-admin/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { OrderComponent } from './order/order.component';
 import { CustomerComponent } from './customer/customer.component';
 import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { AddProductComponent } from './product-detail/add-product/add-product.component';
+import { EditProductComponent } from './product-detail/edit-product/edit-product.component';
+import { AddCategoryComponent } from './category-detail/add-category/add-category.component';
+import { EditCategoryComponent } from './category-detail/edit-category/edit-category.component';
 
 const routes: Routes = [
   { path: "", component:DashboardComponent},
@@ -15,7 +19,11 @@ const routes: Routes = [
   { path: "admin-category", component: CategoryComponent},
   { path: "admin-order", component:OrderComponent},
   { path: "customer-detail", component:CustomerDetailComponent},
-  { path: "order-detail", component:OrderDetailComponent}
+  { path: "order-detail", component:OrderDetailComponent},
+  { path: "admin-product-add", component:AddProductComponent},
+  { path: "admin-product-edit", component:EditProductComponent},
+  { path: "admin-category-add", component:AddCategoryComponent},
+  { path: "admin-category-edit", component:EditCategoryComponent}
 ];
 
 @NgModule({
