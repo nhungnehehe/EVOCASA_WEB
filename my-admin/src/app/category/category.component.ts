251f45e8791444d5a5dd0747bfd765dd0d99d7a5
ThuyTrang1604 <trangptt22411c@st.uel.edu.vn>
2025-03-11 00:27:22 +0700

    category commit

--- my-admin/src/app/category/category.component.ts
+++ my-admin/src/app/category/category.component.ts
@@ -4,8 +4,37 @@ import { Component } from '@angular/core';
   selector: 'app-category',
   standalone: false,
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrl: './category.component.css',
 })
 export class CategoryComponent {
-
+  categories = [
+    {
+      image: 123450,
+      name: 'Soft Goods',
+      id: 'PC001',
+      description:
+        'Soft goods is made from  raw silk fabric on one side. It includes a plush down feather blend insert for exceptional comfort and support.',
+    },
+    {
+      image: 123450,
+      name: 'Lighting',
+      id: 'PC002',
+      description:
+        'Explore our curated collection of vintage and artisanal lamps, designed to add warmth, character, and elegance to any space.',
+    },
+    {
+      image: 123450,
+      name: 'Furniture',
+      id: 'PC003',
+      description:
+        'A curated selection of timeless furniture, blending history and craftsmanship to bring character and sophistication to your space.',
+    },
+    {
+      image: 123450,
+      name: 'Furniture',
+      id: 'PC003',
+      description:
+        'A curated selection of timeless furniture, blending history and craftsmanship to bring character and sophistication to your space.',
+    },
+  ];
 }
