40ee0afcfea8fca9a821e32f661cd4d999808ba5
Nhung Pham Tuyet <131536332+nhungnehehe@users.noreply.github.com>
2025-03-13 09:47:13 +0700

    initial commit

--- my-server/index.js
+++ my-server/index.js
@@ -690,36 +690,6 @@ app.put('/reset-password', cors(), async (req, res) => {
     }
   }
 });
-app.get("/orders", async (req, res) => {
-  try {
-    const result = await orderCollection.find({}).toArray();
-    res.status(200).send(result);
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    res.status(500).send({ error: "Error fetching products" });
-  }
-});
-app.get("/orders/:id", cors(), async (req, res) => {
-  var o_id = new ObjectId(req.params["id"]);
-  const result = await orderCollection.find({ _id: o_id }).toArray();
-  res.send(result[0]);
-});
-app.post("/orders", cors(), async (req, res) => {
-  //put json Order into database 
-  await orderCollection.insertOne(req.body)
-  //send message to client(send all database to client) 
-  res.send(req.body)
-})
-app.delete("/orders/:id", cors(), async (req, res) => {
-  //find detail Order with id 
-  var o_id = new ObjectId(req.params["id"]);
-  const result = await orderCollection.find({ _id: o_id }).toArray();
-  //update json Order into database 
-  await orderCollection.deleteOne(
-    { _id: o_id }
-  )
-  res.send(result[0])
-}) 
 
 // ===== ADMIN APIS =====
 // Get admin by ID
