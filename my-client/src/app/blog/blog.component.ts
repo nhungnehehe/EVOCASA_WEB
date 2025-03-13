4f281ae1b3229cf0606487cec3765969d446d268
Nhung Pham Tuyet <131536332+nhungnehehe@users.noreply.github.com>
2025-03-13 11:57:42 +0700

    Update blog.component.ts

--- my-client/src/app/blog/blog.component.ts
+++ my-client/src/app/blog/blog.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
@@ -6,5 +6,32 @@ import { Component} from '@angular/core';
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css',
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+
+  truncateContent(content: string): string {
+    return content.slice(0, 200) + '...';
+  }
+
+  ngOnInit() {
+    this.initScrollReveal();
+  }
+
+  private initScrollReveal() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    document.querySelectorAll('.reveal').forEach((element) => {
+      observer.observe(element);
+    });
+  }
 }
