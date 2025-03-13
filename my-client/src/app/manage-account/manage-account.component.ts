c4526e732d2c40c8a49bf3d8f4ba60dd65976e75
Thanh Huyen <thhuyenn19@gmail.com>
2025-03-13 15:56:56 +0700

    initial commit

--- my-client/src/app/manage-account/manage-account.component.ts
+++ my-client/src/app/manage-account/manage-account.component.ts
@@ -15,17 +15,13 @@ export class ManageAccountComponent {
   ) {}
    // Hàm xử lý đăng xuất
    signOut(): void {
-    // Xóa token hoặc thông tin đăng nhập từ localStorage
     localStorage.removeItem('token');
     localStorage.removeItem('user');
     
-    // Xóa thông tin người dùng hiện tại từ service
-    this.userService.clearCurrentUser(); // Đảm bảo UserService có phương thức này
+    this.userService.clearCurrentUser(); 
     
-    // Chuyển hướng người dùng về trang đăng nhập
-    this.router.navigate(['/login-page']);
-    
-    // Hiển thị thông báo đăng xuất thành công (tùy chọn)
+    this.router.navigate(['/']);
+
     alert('You have been signed out successfully');
   }
 
