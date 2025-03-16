2ff4d9a01ebef988d77d0fe08e24e941f12d34bf
Nhung Pham Tuyet <131536332+nhungnehehe@users.noreply.github.com>
2025-03-16 17:40:18 +0700

    initial commit

--- my-client/src/app/payment-notice/payment-notice.component.ts
+++ my-client/src/app/payment-notice/payment-notice.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { CustomerService } from '../services/customer.service';
 
 @Component({
   selector: 'app-payment-notice',
@@ -7,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './payment-notice.component.css'
 })
 export class PaymentNoticeComponent {
+  currentUserName: string | null = null;
+  currentUserPhone: string | null = null;
+  customerInfo: any;
 
+  constructor(
+      private userService: UserService,
+      private customerService: CustomerService,
+    ) {}
+  ngOnInit() {
+    // Subscribe vào currentUserPhone$
+    this.userService.currentUserPhone$.subscribe((phone: string) => {
+      this.currentUserPhone = phone;
+      if (this.currentUserPhone) {
+        // Sau khi lấy số điện thoại, gọi API để lấy thông tin khách hàng
+        this.customerService.getCustomerByPhone(this.currentUserPhone).subscribe(
+          (data: any) => {
+            this.customerInfo = data.data; // Lưu thông tin khách hàng vào biến
+            this.currentUserName = this.customerInfo.Name; // Lưu tên người dùng
+
+          },
+          (error) => {
+            console.error("Lỗi khi lấy thông tin khách hàng:", error);
+          }
+        );
+      }
+    });
+  }
 }
